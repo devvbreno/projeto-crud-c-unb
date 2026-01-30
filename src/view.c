2aaed7a5a7fdfaec17997f347685e8a4c43c3677
Samuel Campos Rocha <samuelcamposrocha5@gmail.com>
2026-01-30 14:57:38 -0300

    Implement product management functions
    Added product management functions for adding and listing products.

--- src/view.c
+++ src/view.c
@@ -89,3 +89,43 @@ void menu_editar_cliente(Cliente *lista) {
     editar_cliente(lista, cpf, novo_nome, novo_telefone);
     printf("Dados atualizados com sucesso!\n");
 }
+
+//
+
+void menu_cadastrar_produto(Produto **lista) {
+    int codigo;
+    char nome[50];
+    float preco;
+
+    printf("\n--- Novo Produto ---\n");
+
+    printf("Digite o Codigo (numero): ");
+    scanf("%d", &codigo);
+    getchar(); // Limpa o buffer do \n deixado pelo scanf
+
+    // Validação de unicidade
+    if (buscar_produto(*lista, codigo) != NULL) {
+        printf("Erro: Codigo ja existe!\n");
+        return;
+    }
+
+    printf("Digite o Nome do Produto: ");
+    ler_texto(nome, 50); // Função auxiliar criada pelo Membro 1
+
+    printf("Digite o Preco: ");
+    scanf("%f", &preco);
+    getchar(); // Limpa buffer novamente
+
+    adicionar_produto(lista, codigo, nome, preco);
+    printf("Produto cadastrado com sucesso!\n");
+}
+
+void menu_listar_produtos(Produto *lista) {
+    printf("\n--- Estoque de Produtos ---\n");
+    Produto *atual = lista;
+    while (atual != NULL) {
+        // %.2f formata o float para 2 casas decimais (dinheiro)
+        printf("Cod: %d | Nome: %s | R$ %.2f\n", atual->codigo, atual->nome, atual->preco);
+        atual = atual->prox;
+    }
+}
