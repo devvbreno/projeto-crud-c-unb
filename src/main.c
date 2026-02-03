96d2e5326d19222fa38f5404cd0fd568f7eeeb23
Samuel Campos Rocha <samuelcamposrocha5@gmail.com>
2026-02-03 11:12:39 -0300

    carregamento e salvamento atualizado.

--- src/main.c
+++ src/main.c
@@ -4,14 +4,19 @@
 #include "../include/view.h"
 
 int main() {
-    // inicializa as listas
+    // 1. Inicializa ponteiros como NULL (Lista Vazia)
     Cliente *lista_c = NULL;
     Produto *lista_p = NULL;
 
-    // o loop do menu deve ser chamado aqui
+    // 2. Carrega dados do arquivo "database.txt" se existir
+    carregar_dados(&lista_c, &lista_p);
+
+    // 3. Executa o Menu
     menu_principal(&lista_c, &lista_p);
+
+    // 4. Limpa a memória RAM antes de fechar (Boa prática)
+    // Nota: Os dados já foram salvos dentro do menu_principal ao digitar 0
     liberar_sistema(&lista_c, &lista_p);
 
-    printf ("\n Sistema encerrado com seguranca. \n");
     return 0; 
 }
