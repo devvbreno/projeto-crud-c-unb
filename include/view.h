30c8759228c14c119a742fa1642a1c0a400d057b
Samuel Campos Rocha <samuelcamposrocha5@gmail.com>
2026-02-03 11:23:42 -0300

    Add menu_gerenciar_produtos function declaration

--- include/view.h
+++ include/view.h
@@ -9,8 +9,11 @@ void menu_gerenciar_clientes(Cliente **lista);
 void listar_todos_clientes(Cliente *lista);
 void menu_principal(Cliente **lista_c, Produto **lista_p);
 void menu_vendas(Cliente **lista_c, Produto **lista_p);
+void menu_gerenciar_produtos(Produto **lista);
+void menu_vendas(Cliente **lista_c, Produto **lista_p);
 
 void ctrl_adicionar_item(Cliente *lista_c, Produto *lista_p, char *cpf, int cod_prod, int qtd);
 void ctrl_remover_item(Cliente *lista_c, Produto *lista_p, char *cpf, int cod_prod);
 void ctrl_finalizar_venda(Cliente *lista_c, char *cpf, Produto *lista_p);
 #endif
+
